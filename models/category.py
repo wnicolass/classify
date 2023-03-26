996f37f81966d2a0b3c9bc503a686f438787f0d8
wnicolass <nicolaswgl99@gmail.com>
2023-03-26 14:59:24 +0100

    feat: create all_models module to import in create_metadata | create category model

---
+++ models/category.py
@@ -0,0 +1,16 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+from sqlalchemy.orm import relationship
+from config.database import Base
+
+class Category(Base):
+    __tablename__ = "Category"
+
+    category_id: int = Column(Integer, primary_key = True, autoincrement = True)
+    name: str = Column(String(30), nullable = False, index = True)
+
+    subcategories = relationship('Subcategory', cascade = 'delete')
+
