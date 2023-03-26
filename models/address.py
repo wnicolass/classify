821e77a2a0ef83e8000ab7f67b5b64f34f8cc577
wnicolass <nicolaswgl99@gmail.com>
2023-03-26 18:09:10 +0100

    fix: change user reference to user_id instead of id

--- models/address.py
+++ models/address.py
@@ -17,7 +17,7 @@ class Address(Base):
     city: str = Column(String(40), nullable = False, index = True)
     street: str = Column(String(60), nullable = False)
     number: int = Column(Integer, nullable = False)
-    user_id: int = Column(Integer, ForeignKey("User.id", ondelete = 'CASCADE'), nullable = False)
+    user_id: int = Column(Integer, ForeignKey("User.user_id", ondelete = 'CASCADE'), nullable = False)
     address_date: datetime = Column(DateTime, default = datetime.now())
 
     user = relationship("User", back_populates = "address")
