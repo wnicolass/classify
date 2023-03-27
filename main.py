b1fed58e732f7ccab042de7234f00b151af34104
wnicolass <nicolaswgl99@gmail.com>
2023-03-27 20:39:50 +0100

    refactor: restructure the code and add cli option to seed initial data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import typer
 from fastapi import FastAPI
 from config.database import create_metadata
+from data.seed import seed_data
 
 app = FastAPI()
 
@@ -8,31 +9,34 @@ def main():
     typer.run(start_uvicorn)
 
 def start_uvicorn(
-        create_ddl: bool = typer.Option(False, "--create-ddl", "-c", help = "Drop and Create all tables. [default: False]"), 
-        auto_reload: bool = typer.Option(False, "--auto-reload", "-r", help = "Define if the app will reload after each saving. [default: False]"),
         host: str = typer.Argument("127.0.0.1", help = "IP Address in which the server will be executed."),
-        port: int = typer.Option(8000, "--port", "-p", help = "Port in which the server will listen.")
+        auto_reload: bool = typer.Option(False, "--auto-reload", "-r", help = "Define if the app will reload after each saving. [default: False]"),
+        port: int = typer.Option(8000, "--port", "-p", help = "Port in which the server will listen."),
+        create_ddl: bool = typer.Option(False, "--create-ddl", "-c", help = "Drop and Create all tables. [default: False]"),
+        seed: bool = typer.Option(False, "--create-dml", "-d", help = "Insert dummy data into database. [default: False]")
     ):
     """
     Classify is a classifieds webapp built with FastAPI & SQLAlchemy.
     """
-    import asyncio
-    import platform
     import uvicorn
     from rich import print
     from tqdm import tqdm
     from time import sleep
     
     if create_ddl:
-        if platform.system() == 'Windows':
-            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         print('[bold red]Creating metadata...[/bold red]')
-        
-        for i in tqdm(range(0, 100), desc = 'Adding...'):
+        for i in tqdm(range(0, 100), desc = 'CREATE', colour = '#FF0000'):
             sleep(0.03)
-        asyncio.run(create_metadata())
-
+        create_metadata()
         print('[bold green]Metadata created successfully![/bold green]')
+        if seed:
+            print('[bold red]Inserting initial data...[/bold red]')
+            for i in tqdm(range(0, 100), desc = 'INSERT', colour = '#00FF00'):
+                sleep(0.03)
+            seed_data()
+            print('[bold green]Data inserted successfully![/bold green]')
+
+
 
     uvicorn.run(
         'main:app',
