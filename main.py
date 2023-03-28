97d72939b20b2ac82c782384302ac04d8192c79e
wnicolass <nicolaswgl99@gmail.com>
2023-03-28 13:59:22 +0100

    feat: split code and execute store procedure for seeding

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import typer
 from fastapi import FastAPI
 from config.database import create_metadata
+from data.seed import seed_data
 
 app = FastAPI()
 
 def main():
     typer.run(start_uvicorn)
 
+async def seeding(ddl, dml) -> None:
+    from rich import print
+    from tqdm import tqdm
+    from time import sleep
+
+    if ddl:
+        print('[bold red]Creating metadata...[/bold red]')
+        
+        for i in tqdm(range(0, 100), desc = 'CREATE', colour = '#0000FF'):
+            sleep(0.03)
+        await create_metadata()
+
+        print('[bold green]Metadata created successfully![/bold green]')
+        if dml:
+            print('[bold red]Inserting initial data...[/bold red]')
+            for i in tqdm(range(0, 100), desc = 'INSERT', colour = '#00FF00'):
+                sleep(0.03)
+
+            await seed_data()
+            print('[bold red]Initial data inserted successfully...[/bold red]')
+
 def start_uvicorn(
-        create_ddl: bool = typer.Option(False, "--create-ddl", "-c", help = "Drop and Create all tables. [default: False]"), 
+        create_ddl: bool = typer.Option(False, "--create-ddl", "-c", help = "Drop and Create all tables. [default: False]"),
+        create_dml: bool = typer.Option(False, '--crete-dml', '-d', help = "Insert initil data into database."),
         auto_reload: bool = typer.Option(False, "--auto-reload", "-r", help = "Define if the app will reload after each saving. [default: False]"),
         host: str = typer.Argument("127.0.0.1", help = "IP Address in which the server will be executed."),
         port: int = typer.Option(8000, "--port", "-p", help = "Port in which the server will listen.")
@@ -16,23 +39,14 @@ def start_uvicorn(
     """
     Classify is a classifieds webapp built with FastAPI & SQLAlchemy.
     """
+    import uvicorn
     import asyncio
     import platform
-    import uvicorn
-    from rich import print
-    from tqdm import tqdm
-    from time import sleep
     
     if create_ddl:
         if platform.system() == 'Windows':
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        print('[bold red]Creating metadata...[/bold red]')
-        
-        for i in tqdm(range(0, 100), desc = 'Adding...'):
-            sleep(0.03)
-        asyncio.run(create_metadata())
-
-        print('[bold green]Metadata created successfully![/bold green]')
+        asyncio.run(seeding(create_ddl, create_dml))
 
     uvicorn.run(
         'main:app',
