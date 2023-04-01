49364176acb0f498a2bcb3bbf9f0ec06bbf267d4
wnicolass <nicolaswgl99@gmail.com>
2023-04-01 01:42:37 +0100

    feaet: add some utilities functions for validation

---
+++ common/utils.py
@@ -0,0 +1,40 @@
+from datetime import date
+import re
+
+MIN_DATE = date.fromisoformat('1920-01-01')
+
+def is_valid_iso_date(iso_date: str) -> bool:
+    try:
+        date.fromisoformat(iso_date)
+    except ValueError:
+        return False
+    else:
+        return True
+    
+def is_valid_birth_date(birth_date: str) -> bool:
+        return (is_valid_iso_date(birth_date) and date.fromisoformat(birth_date) >= MIN_DATE and date.fromisoformat(birth_date) <= date.today())
+
+def make_test_regex_fn(regex: str):
+    compiled_regex = re.compile(regex)
+    def test_regex_fn(value: str) -> bool:
+        return bool(re.fullmatch(compiled_regex, value))
+    return test_regex_fn
+
+def handle_phone(phone_number: str) -> str:
+    return re.sub(r'\+|\s', '', phone_number.strip())
+
+is_valid_email = make_test_regex_fn(
+    r"[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"
+)
+
+is_valid_password = make_test_regex_fn(
+     r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_\-+={[}\]|\\:;"\'<,>.?/]).{6,20}'
+)
+
+is_valid_username = make_test_regex_fn(
+    r'^(?!.*\d)(?!.*[~`!@#$%^&*()_\-+={[}\]|\\:;"\'<,>.?/]).*'
+)
+
+is_valid_phone_number = make_test_regex_fn(
+    r'(^\d{9}$)|(^\+\d{12}$)'
+)
