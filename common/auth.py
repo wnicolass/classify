aacfca1a6b2235ae1d306b06b792742a6314ef78
wnicolass <nicolaswgl99@gmail.com>
2023-04-02 03:00:41 +0100

    feat: add functions to set cookie and get user auth status from cookie

--- common/auth.py
+++ common/auth.py
@@ -1,9 +1,64 @@
+import os
+from typing import Optional
+from dotenv import (
+    load_dotenv, 
+    find_dotenv
+)
+from hashlib import sha512
+from fastapi import Request, Response
 from passlib.context import CryptContext
+from models.user import UserAccount, UserLoginData
+from middlewares.global_request import global_request
+
+load_dotenv(find_dotenv())
 
 hash_context = CryptContext(schemes = ['bcrypt'], deprecated = 'auto')
+SECRET = os.getenv('COOKIE_SECRET')
+current_user: Optional[UserLoginData] = None
 
 def check_password(password: str, hashed_password: str) -> bool:
     return hash_context.verify(password, hashed_password)
 
 def hash_password(password: str) -> str:
     return hash_context.hash(password)
+
+def set_auth_cookie(response: Response, user: UserLoginData):
+    global current_user
+    cookie_value = f'{str(user.user_id)}:{hash_cookie_value(str(user.user_id))}'
+
+    current_user = user
+    response.set_cookie(
+        os.getenv('COOKIE_NAME'),
+        cookie_value,
+        secure = False,
+        httponly = True,
+        samesite = 'lax',
+        max_age = os.getenv('COOKIE_MAX_AGE')
+    )
+
+def delete_auth_cookie(response: Response):
+    response.delete_cookie(os.getenv('COOKIE_NAME'))
+
+def hash_cookie_value(cookie_value: str) -> str:
+    return sha512(f'{cookie_value}{SECRET}'.encode('utf-8')).hexdigest()
+
+def get_auth_from_cookie(request: Request) -> int | None:
+    if not (cookie_value:= request.cookies.get(os.getenv('COOKIE_NAME'))):
+        return None
+    
+    cookie_parts = cookie_value.split(':')
+    if len(cookie_parts) != 2:
+        return None
+    
+    user_id, hash_value_from_cookie = cookie_parts
+    hash_for_check = hash_cookie_value(user_id)
+    if hash_for_check != hash_value_from_cookie:
+        return None
+    
+    return int(user_id) if user_id.isdigit() else None
+
+def get_current_auth_user() -> UserAccount | None:
+    if user_id := get_auth_from_cookie(global_request.get()):
+        return user_id if current_user.user_id == user_id else None
+    return None
+#:
--- common/viewmodel.py
+++ common/viewmodel.py
@@ -1,14 +1,14 @@
 from typing import Any
+from common.auth import get_current_auth_user
 
 class ViewModel(dict):
     def __init__(self, *args, **kargs):
-        # user = get_current_user()
-        # user_id = user.id if user else None
+        user_id = get_current_auth_user()
         all = {
             'error': None,
             'error_msg': None,
-            # 'user_id': user_id,
-            # 'is_logged_in': user_id is not None
+            'user_id': user_id,
+            'is_logged_in': user_id is not None
         }
         all.update(kargs)
         super().__init__(self, *args, **all)
