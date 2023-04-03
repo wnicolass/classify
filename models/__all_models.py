fe0dcde478dfcb3b896b6a614ed391fcba409596
wnicolass <nicolaswgl99@gmail.com>
2023-04-04 00:51:48 +0100

    feat: use association object to also store token id into database

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,5 +1,5 @@
 from models.category import Category
-from models.user import (UserAccount, UserLoginData, HashAlgo, EmailValidationStatus, ExternalProvider, UserAddress)
+from models.user import (UserAccount, UserLoginData, HashAlgo, EmailValidationStatus, ExternalProvider, UserAddress, UserLoginDataExt)
 from models.admin import AdminAccount
 from models.subcategory import Subcategory, FieldDefinition
 from models.ad import (Ad, AdAddress, AdCondition, AdStatus, AdImage, Feature)
