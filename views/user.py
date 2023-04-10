a52ccaf5d1d13885fcf5023bac6f1955856ec746
Steelux8 <steelux7@gmail.com>
2023-04-10 19:34:21 +0100

    feat: all profile pages redirect if not auth

--- views/user.py
+++ views/user.py
@@ -10,30 +10,55 @@ from common.auth import requires_authentication
 
 router = APIRouter()
 
-@router.get('/user/dashboard', dependencies = [Depends(requires_authentication)])
+@router.get('/user/dashboard')
 @template()
 async def dashboard():
-    return await ViewModel()
+    vm = await ViewModel()
 
-@router.get('/user/profile-settings', dependencies = [Depends(requires_authentication)])
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
+
+@router.get('/user/profile-settings')
 @template('user/profile-settings.pt')
 async def profile_settings():
-    return await ViewModel()
+    vm = await ViewModel()
 
-@router.get('/user/my-ads', dependencies = [Depends(requires_authentication)])
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
+
+@router.get('/user/my-ads')
 @template('user/my-ads.pt')
 async def my_ads():
-    return await ViewModel()
+    vm = await ViewModel()
 
-@router.get('/user/offermessages', dependencies = [Depends(requires_authentication)])
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
+
+@router.get('/user/offermessages')
 @template()
 async def offermessages():
-    return await ViewModel()
+    vm = await ViewModel()
 
-@router.get('/user/payments', dependencies = [Depends(requires_authentication)])
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
+
+@router.get('/user/payments')
 @template()
 async def payments():
-    return await ViewModel()
+    vm = await ViewModel()
+
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
 
 @router.get('/user/favourite-ads')
 @template('user/favourite-ads.pt')
@@ -45,7 +70,12 @@ async def favourite_ads():
         return response
     return vm
 
-@router.get('/user/privacy-setting', dependencies = [Depends(requires_authentication)])
+@router.get('/user/privacy-setting')
 @template('user/privacy-setting.pt')
 async def privacy_setting():
-    return await ViewModel()
+    vm = await ViewModel()
+
+    if not vm.is_logged_in:
+        response = responses.RedirectResponse(url = '/auth/sign-in', status_code = status.HTTP_302_FOUND)
+        return response
+    return vm
