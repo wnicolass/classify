42dd92c36ba3a491f1c02d4d0ec445b3ec3137e2
Steelux8 <steelux7@gmail.com>
2023-04-11 18:30:50 +0100

    feat: validate details and manage phone number

--- views/user.py
+++ views/user.py
@@ -15,6 +15,12 @@ from common.fastapi_utils import get_db_session, form_field_as_str
 from sqlalchemy.ext.asyncio import AsyncSession
 from services import user_service
 from models.user import UserAccount
+from common.utils import (
+    is_valid_birth_date, 
+    is_valid_username,
+    is_valid_phone_number,
+    handle_phone
+)
 
 router = APIRouter()
 
@@ -43,14 +49,15 @@ async def profile_settings(
     user = await get_current_auth_user()
     vm = await profile_settings_viewmodel(request, session, user)
     
-    if vm.error:
-        return vm
-    
-    template = PageTemplateFile('./templates/user/profile-settings.pt')
     vm.name = user.username
     vm.email = ''
     vm.phone_number = user.phone_number
     vm.birth_date = user.birth_date
+    
+    if vm.error:
+        return vm
+    
+    template = PageTemplateFile('./templates/user/profile-settings.pt')
     content = template(**vm)
     return responses.HTMLResponse(content, status_code = status.HTTP_200_OK)
 
@@ -66,10 +73,17 @@ async def profile_settings_viewmodel(
         new_phone_number = form_field_as_str(form_data, 'phone_number'),
         new_birth_date = form_field_as_str(form_data, 'birth_date')
     )
+    
+    if not is_valid_username(vm.new_username):
+        vm.error, vm.error_msg = True, 'Username inválido!'
+    elif not is_valid_phone_number(vm.new_phone_number):
+        vm.error, vm.error_msg = True, 'Número de telemóvel inválido!'    
+    elif not is_valid_birth_date(vm.new_birth_date):
+        vm.error, vm.error_msg = True, 'Data de nascimento inválida!'
     if not vm.error:
         if user:
-            await user_service.update_user_details(user, vm.new_username, vm.new_phone_number, vm.new_birth_date, session)
-            vm.success, vm.success_msg = True, 'Senha alterada com sucesso. Faça login para comprar e vender.'
+            await user_service.update_user_details(user, vm.new_username, handle_phone(vm.new_phone_number), vm.new_birth_date, session)
+            vm.success, vm.success_msg = True, 'Dados da conta alterados com sucesso!.'
     
     return vm
 
