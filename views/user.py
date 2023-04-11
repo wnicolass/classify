c0b36985d051cd6f60c1e3196a725bbdff408cb2
Steelux8 <steelux7@gmail.com>
2023-04-11 16:55:17 +0100

    feat: first implementation of details update

--- views/user.py
+++ views/user.py
@@ -1,12 +1,19 @@
+from typing import Annotated
 from fastapi import (
     APIRouter, 
     Depends,
+    Request,
     responses,
     status
 )
 from fastapi_chameleon import template
+from chameleon import PageTemplateFile
 from common.viewmodel import ViewModel
 from common.auth import requires_authentication
+from common.auth import get_current_auth_user
+from common.fastapi_utils import get_db_session, form_field_as_str
+from sqlalchemy.ext.asyncio import AsyncSession
+from services import user_service
 
 router = APIRouter()
 
@@ -18,7 +25,49 @@ async def dashboard():
 @router.get('/user/profile-settings', dependencies = [Depends(requires_authentication)])
 @template('user/profile-settings.pt')
 async def profile_settings():
-    return await ViewModel()
+    user = await get_current_auth_user()
+    return await ViewModel(
+        name = user.username,
+        email = '',
+        phone_number = user.phone_number,
+        birth_date = user.birth_date
+    )
+    
+@router.post('/user/profile-settings')
+@template('user/profile-settings.pt')
+async def profile_settings(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(get_db_session)]
+):
+    vm = await profile_settings_viewmodel(request, session)
+    
+    if vm.error:
+        return vm
+    
+    template = PageTemplateFile('./templates/auth/sign-in.pt')
+    content = template(**vm)
+    return responses.HTMLResponse(content, status_code = status.HTTP_200_OK)
+
+async def profile_settings_viewmodel(
+    request: Request, 
+    session: Annotated[AsyncSession, Depends(get_db_session)]
+):
+    user = await get_current_auth_user()
+    
+    form_data = await request.form()
+    
+    vm = await ViewModel(
+        new_username = form_field_as_str(form_data, 'username'),
+        new_phone_number = form_field_as_str(form_data, 'phone_number'),
+        new_birth_date = form_field_as_str(form_data, 'birth_date')
+    )
+    if not vm.error:
+        if user:
+            print(form_data)
+            await user_service.update_user_details(user, vm.new_username, vm.new_phone_number, vm.new_birth_date, session)
+            vm.success, vm.success_msg = True, 'Senha alterada com sucesso. Faça login para comprar e vender.'
+    
+    return vm
 
 @router.get('/user/my-ads', dependencies = [Depends(requires_authentication)])
 @template('user/my-ads.pt')
