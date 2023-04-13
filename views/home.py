f6a5f69ede41939f7d57204138d1f835ed77a66d
wnicolass <nicolaswgl99@gmail.com>
2023-04-13 23:44:55 +0100

    feat: use services to render ads on the index view

--- views/home.py
+++ views/home.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi_chameleon import template
 from common.fastapi_utils import get_db_session
 from common.viewmodel import ViewModel
-from services import category_service, ad_service
+from services import category_service, ad_service, user_service
 
 router = APIRouter()
 
@@ -26,7 +26,12 @@ async def index_viewmodel(
         popular_categories = await category_service.popular_categories(session),
         all_cities = await ad_service.get_cities_with_ads(session),
         all_categories = await category_service.get_all_categories(session),
-        popular_ads = await ad_service.get_popular_ads(session)
+        popular_ads = await ad_service.get_popular_ads(session),
+        recent_ads = await ad_service.get_ads_by_creation_date(session),
+        total_ads = len(await ad_service.get_all_ads(session)),
+        total_verified_users = len(await user_service.get_all_verified_users(session)),
+        sold_ads = len(await ad_service.get_ads_by_status(session, 'vendido')),
+        top_locations = await ad_service.get_locations_by_total_ads(session)
     )
 
 @router.get('/home/about')
