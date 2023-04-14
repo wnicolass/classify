c056793bb87292e3c34ac8ffa072ba84d3698656
wnicolass <nicolaswgl99@gmail.com>
2023-04-15 00:51:18 +0100

    feat: add current user fav ads

--- views/home.py
+++ views/home.py
@@ -22,17 +22,19 @@ async def index(
 async def index_viewmodel(
         session: Annotated[AsyncSession, Depends(get_db_session)]
 ):
-    return await ViewModel(
-        popular_categories = await category_service.popular_categories(session),
-        all_cities = await ad_service.get_cities_with_ads(session),
-        all_categories = await category_service.get_all_categories(session),
-        popular_ads = await ad_service.get_popular_ads(session),
-        recent_ads = await ad_service.get_ads_by_creation_date(session),
-        total_ads = len(await ad_service.get_all_ads(session)),
-        total_verified_users = len(await user_service.get_all_verified_users(session)),
-        sold_ads = len(await ad_service.get_ads_by_status(session, 'vendido')),
-        top_locations = await ad_service.get_locations_by_total_ads(session)
-    )
+    vm = await ViewModel()
+    vm.current_user_fav_ads = await user_service.get_user_favs(vm.user_id, session)
+    vm.popular_categories = await category_service.popular_categories(session)
+    vm.all_cities = await ad_service.get_cities_with_ads(session)
+    vm.all_categories = await category_service.get_all_categories(session)
+    vm.popular_ads = await ad_service.get_popular_ads(session)
+    vm.recent_ads = await ad_service.get_ads_by_creation_date(session)
+    vm.total_ads = len(await ad_service.get_all_ads(session))
+    vm.total_verified_users = len(await user_service.get_all_verified_users(session))
+    vm.sold_ads = len(await ad_service.get_ads_by_status(session, 'vendido'))
+    vm.top_locations = await ad_service.get_locations_by_total_ads(session)
+
+    return vm
 
 @router.get('/about')
 @template()
