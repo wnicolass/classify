b4a9b5dfd6cb692ed07e221c5950909a52deac45
Diamantino <filipemoura869@gmail.com>
2023-04-18 22:04:57 +0100

    refactor: remove useless code and restructuring code

--- services/category_service.py
+++ services/category_service.py
@@ -7,35 +7,6 @@ from models.subcategory import Subcategory
 from models.ad import Ad
 from services.ad_service import AdStatusEnum
 
-async def get_category_by_id(id: int, session: AsyncSession) -> Category | None:
-    query = await session.execute(select(Category).where(Category.id == id))
-    category = query.scalar_one_or_none()
-    return category
-
-
-async def get_category_by_name(name: str, session: AsyncSession) -> Category | None:
-    query = await session.execute(select(Category).where(Category.category_name == name))
-    category = query.scalar_one_or_none()
-    return category
-
-async def get_categories_by_asc(session: AsyncSession) -> List[Category]:
-    query = await session.execute(select(Category).order_by(Category.category_name.asc()))
-    asc_categories = query.scalars().all()
-
-    return asc_categories
-
-async def get_categories_by_desc(session: AsyncSession) -> List[Category]:
-    query = await session.execute(select(Category).order_by(Category.category_name.desc()))
-    desc_categories = query.scalars().all()
-
-    return desc_categories
-
-async def get_category_count(session: AsyncSession) -> int | None:
-    query = await session.execute(select(func.count(Category.id)))
-    category_count = query.scalar_one_or_none()
-    return category_count
-
-
 async def get_all_categories(session: AsyncSession) -> List[Category]:
     query = await session.execute(select(
             Category)
@@ -47,7 +18,6 @@ async def get_all_categories(session: AsyncSession) -> List[Category]:
     all_categories = query.unique().scalars().all()
     
     return all_categories
-
 
 async def popular_categories(session: AsyncSession) -> List[Category]:
     query = await session.execute(select(
@@ -61,30 +31,6 @@ async def popular_categories(session: AsyncSession) -> List[Category]:
     )
     popular_categories = query.unique().scalars().all()
     return popular_categories
-
-
-async def create_category(category_name: str, session: AsyncSession) -> Category | None:
-    category = Category(
-        category_name = category_name
-    )
-    
-    session.add(category)
-    await session.commit()
-    await session.refresh(category)
-    return category
-
-
-
-async def get_subcategory_by_id(id: int, session: AsyncSession) -> Subcategory | None:
-    query = await session.execute(select(Subcategory).where(Subcategory.id == id))
-    subcategory = query.scalar_one_or_none()
-    return subcategory
-
-
-async def get_subcategory_by_name(name: str, session: AsyncSession) -> Subcategory | None:
-    query = await session.execute(select(Subcategory).where(Subcategory.subcategory_name == name))
-    subcategory = query.scalar_one_or_none()
-    return subcategory
 
 async def get_subcategory_by_category_id(category_id: int, session: AsyncSession) -> List[Subcategory]:
     query = await session.execute(select(Subcategory).where(Subcategory.category_id == category_id))
@@ -100,26 +46,3 @@ async def get_category_by_subcategory_id(subcategory_id: int, session: AsyncSess
     category = query.unique().scalar_one_or_none()
 
     return category.id
-
-async def get_subcategory_count(session: AsyncSession) -> int | None:
-    query = await session.execute(select(func.count(Subcategory.id)))
-    subcategory_count = query.scalar_one_or_none()
-    return subcategory_count
-
-
-async def get_all_subcategories(session: AsyncSession) -> List[Subcategory]:
-    query = await session.execute(select(Subcategory))
-    all_subcategories = query.scalars().all()
-    return all_subcategories
-
-
-async def create_subcategory(subcategory_name: str, category_id, session: AsyncSession) -> Subcategory | None:
-    subcategory = Subcategory(
-        subcategory_name = subcategory_name,
-        category_id = category_id
-    )
-    
-    session.add(subcategory)
-    await session.commit()
-    await session.refresh(subcategory)
-    return subcategory
--- services/user_service.py
+++ services/user_service.py
@@ -6,85 +6,7 @@ from sqlalchemy.orm import joinedload
 from models.user import UserAccount, UserLoginData, UserLoginDataExt, Favourite
 from models.ad import Ad
 
-async def get_user_by_email(email: str, session: AsyncSession) -> UserLoginData | None:
-    query = await session.execute(select(UserLoginData).where(UserLoginData.email_addr == email))
-    user = query.scalar_one_or_none()
-    
-    return user
-
-async def get_user_by_id(id: int, session: AsyncSession) -> UserLoginData:
-    query = await session.execute(select(UserLoginData).where(UserLoginData.user_id == id))
-    user = query.scalar_one_or_none()
-    
-    return user
-
-async def get_user_account_by_id(id: int, session: AsyncSession) -> UserAccount:
-    query = await session.execute(select(UserAccount)
-        .where(UserAccount.user_id == id)
-        .options(joinedload(UserAccount.favourites))
-    )
-    user = query.unique().scalar_one_or_none()
-    return user
-
-async def get_all_verified_users(session: AsyncSession) -> List[UserAccount]:
-    query = await session.execute(select(UserAccount).where(UserAccount.is_active == 1))
-    verified_users = query.unique().scalars().all()
-
-    return verified_users
-
-async def get_user_favs(user_id: int, session: AsyncSession) -> List[int]:
-    query = await session.execute(select(Favourite.ad_id)
-        .where(Favourite.user_id == user_id)
-    )
-    user_favs_ids = query.unique().scalars().all()
-    return user_favs_ids
-
-async def set_email_confirmation_token(user_id: int, token: str, expiration_time: datetime, session: AsyncSession):
-    user = await get_user_by_id(user_id, session)
-    user.confirm_token = token
-    user.confirm_token_time = expiration_time
-    await session.commit()
-
-async def set_user_recovery_token(user: UserLoginData, recovery_token_hash: str, recovery_token_time: datetime, session: AsyncSession):
-    user = await get_user_by_id(user.user_id, session)
-    user.recovery_token = recovery_token_hash
-    user.recovery_token_time = recovery_token_time
-    await session.commit()
-
-async def get_user_by_recovery_token(token: str, session: AsyncSession)-> UserLoginData:
-    result = await session.execute(select(UserLoginData).where(UserLoginData.recovery_token == token))
-    user = result.scalar_one_or_none()
-
-    return user
-
-async def get_user_by_email_token(token: str, session: AsyncSession) -> UserLoginData:
-    result = await session.execute(select(UserLoginData).where(UserLoginData.confirm_token == token))
-    user = result.scalar_one_or_none()
-
-    return user
-
-async def get_user_by_google_hash(token: str, session: AsyncSession) -> UserLoginDataExt:
-    result = await session.execute(select(UserLoginDataExt).where(UserLoginDataExt.external_provider_token == token))
-    user_ext_data = result.scalar_one_or_none()
-    return user_ext_data
-
-async def update_user_details(user: UserAccount, new_username: str, new_phone_number: str, new_birth_date: str, session: AsyncSession):
-    db_user = await get_user_account_by_id(user.user_id, session)
-    db_user.username = new_username
-    db_user.phone_number = new_phone_number
-    db_user.birth_date = new_birth_date
-    await session.commit()
-
-async def update_user_email_validation_status(user: UserLoginData, session: AsyncSession):
-    user.email_validation_status_id = 3
-    user.user.is_active = 1
-    await session.commit()
-
-async def update_user_password(user: UserLoginData, new_hashed_password: str, new_password_salt: str, session: AsyncSession):
-    user.password_hash = new_hashed_password
-    user.password_salt = new_password_salt
-    await session.commit()
-
+# CREATE
 async def create_user(username: str, phone_number: str, birth_date: str, is_active: int, session: AsyncSession, image_url: str | None = None) -> UserAccount:
     user = UserAccount(
         username = username,
@@ -127,6 +49,88 @@ async def add_new_favourite(user_id: int, ad_id: int, session: AsyncSession) -> 
     
     return fav
 
+# READ
+async def get_user_by_email(email: str, session: AsyncSession) -> UserLoginData | None:
+    query = await session.execute(select(UserLoginData).where(UserLoginData.email_addr == email))
+    user = query.scalar_one_or_none()
+    
+    return user
+
+async def get_user_by_id(id: int, session: AsyncSession) -> UserLoginData:
+    query = await session.execute(select(UserLoginData).where(UserLoginData.user_id == id))
+    user = query.scalar_one_or_none()
+    
+    return user
+
+async def get_user_account_by_id(id: int, session: AsyncSession) -> UserAccount:
+    query = await session.execute(select(UserAccount)
+        .where(UserAccount.user_id == id)
+        .options(joinedload(UserAccount.favourites))
+    )
+    user = query.unique().scalar_one_or_none()
+    return user
+
+async def get_all_verified_users(session: AsyncSession) -> List[UserAccount]:
+    query = await session.execute(select(UserAccount).where(UserAccount.is_active == 1))
+    verified_users = query.unique().scalars().all()
+
+    return verified_users
+
+async def get_user_favs(user_id: int, session: AsyncSession) -> List[int]:
+    query = await session.execute(select(Favourite.ad_id)
+        .where(Favourite.user_id == user_id)
+    )
+    user_favs_ids = query.unique().scalars().all()
+    return user_favs_ids
+
+
+async def get_user_by_recovery_token(token: str, session: AsyncSession)-> UserLoginData:
+    result = await session.execute(select(UserLoginData).where(UserLoginData.recovery_token == token))
+    user = result.scalar_one_or_none()
+
+    return user
+
+async def get_user_by_email_token(token: str, session: AsyncSession) -> UserLoginData:
+    result = await session.execute(select(UserLoginData).where(UserLoginData.confirm_token == token))
+    user = result.scalar_one_or_none()
+
+    return user
+
+async def get_user_by_google_hash(token: str, session: AsyncSession) -> UserLoginDataExt:
+    result = await session.execute(select(UserLoginDataExt).where(UserLoginDataExt.external_provider_token == token))
+    user_ext_data = result.scalar_one_or_none()
+    return user_ext_data
+
+# UPDATE/DELETE
+async def set_email_confirmation_token(user_id: int, token: str, expiration_time: datetime, session: AsyncSession):
+    user = await get_user_by_id(user_id, session)
+    user.confirm_token = token
+    user.confirm_token_time = expiration_time
+    await session.commit()
+
+async def set_user_recovery_token(user: UserLoginData, recovery_token_hash: str, recovery_token_time: datetime, session: AsyncSession):
+    user = await get_user_by_id(user.user_id, session)
+    user.recovery_token = recovery_token_hash
+    user.recovery_token_time = recovery_token_time
+    await session.commit()
+
+async def update_user_details(user: UserAccount, new_username: str, new_phone_number: str, new_birth_date: str, session: AsyncSession):
+    db_user = await get_user_account_by_id(user.user_id, session)
+    db_user.username = new_username
+    db_user.phone_number = new_phone_number
+    db_user.birth_date = new_birth_date
+    await session.commit()
+
+async def update_user_email_validation_status(user: UserLoginData, session: AsyncSession):
+    user.email_validation_status_id = 3
+    user.user.is_active = 1
+    await session.commit()
+
+async def update_user_password(user: UserLoginData, new_hashed_password: str, new_password_salt: str, session: AsyncSession):
+    user.password_hash = new_hashed_password
+    user.password_salt = new_password_salt
+    await session.commit()
+    
 async def delete_user_favourite(current_user: UserAccount, fav: Favourite, session: AsyncSession) -> None:
     await session.delete(fav)
     await session.commit()
