dfda191fbb4b50cb0439d76ac93d5876b2f1c238
wnicolass <nicolaswgl99@gmail.com>
2023-04-24 13:57:09 +0100

    feat: end script that handles payments

--- public/scripts/promo-handler.js
+++ public/scripts/promo-handler.js
@@ -1,8 +1,56 @@
+const premiumCard = document.getElementById('premium-card');
 let selectedAd;
+
+function handlePromoOptions(promoId) {
+    if (promoId === 2) {
+        premiumCard.style.display = 'none';
+        const modalContent = document.querySelector('.page-content');
+        modalContent.style.gridTemplateColumns = '1fr'
+        return;
+    }
+    premiumCard.style.display = 'block';
+    const modalContent = document.querySelector('.page-content');
+    modalContent.style.gridTemplateColumns = 'repeat(2, 1fr)';
+}
 
 function setAdIdToPromoButton({currentTarget}) {
     selectedAd = +currentTarget.dataset.adid;
-    console.log(selectedAd);
+    currentAdPromoId = +currentTarget.dataset.promoid;
+
+    handlePromoOptions(currentAdPromoId);
+}
+
+async function upgradeAdPromo({currentTarget: promoBtn}) {
+    const selectedPromo = +promoBtn.dataset.promoid;
+    
+    try {
+        const res = await fetch('/ad/checkout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ad_id: selectedAd,
+                promo_id: selectedPromo
+            })
+        });
+        const stripeUrl = await res.json();
+        
+        if (!res.ok) {
+            return alert('Something went wrong!');
+        }
+        
+        window.location.href = stripeUrl;
+    } catch(err) {
+        console.error(err.message);
+        alert('Something went wrong!');
+    }
+}
+
+function setFetchEvent() {
+    const promoButtons = document.querySelectorAll('button[data-promoid]');
+    
+    promoButtons.forEach(btn => btn.addEventListener('click', upgradeAdPromo));
 }
 
 function main() {
@@ -11,6 +59,8 @@ function main() {
     allSelectPromoBtns.forEach(btn => {
         btn.addEventListener('click', setAdIdToPromoButton);
     });
+
+    setFetchEvent();
 }
 
 window.addEventListener('load', main);
