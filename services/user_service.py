e65e91f6f01d6d305941940563719a0027c890d9
Diamantino <filipemoura869@gmail.com>
2023-04-28 18:10:23 +0100

    feat: add new queries related to chat and messages

--- services/user_service.py
+++ services/user_service.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List
-from sqlalchemy import and_
+from sqlalchemy import and_, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload
@@ -197,7 +197,11 @@ async def send_message(
 ):
     chatroom = await get_chatroom_by_seller_and_buyer_id(seller_user_id, buyer_user_id, session)
     if not chatroom:
-        chatroom = Chatroom(ad_id = adv_id)
+        chatroom = Chatroom(
+            ad_id = adv_id, 
+            is_unread_receiver_user = 1,
+            is_unread_sender_user = 0
+        )
         session.add(chatroom)
         await session.commit()
         await session.refresh(chatroom)
@@ -222,16 +226,67 @@ async def get_chatroom_by_seller_and_buyer_id(
         select(Chatroom)
         .join(Chatroom.messages)
         .where(
-            and_(
+            or_(and_(
                 Message.sender_user_id == buyer_user_id,
                 Message.receiver_user_id == seller_user_id,
+            ),
+            and_(
+                Message.sender_user_id == seller_user_id,
+                Message.receiver_user_id == buyer_user_id,
             )
-        )
+        ))
         .limit(1)
     )
     chatroom = query.scalar_one_or_none()
 
     return chatroom
+
+async def get_chatroom_by_id(
+    chatroom_id: int,
+    session: AsyncSession
+):
+    query = await session.execute(
+        select(Chatroom)
+        .where(Chatroom.id == chatroom_id)
+    )
+    chatroom = query.scalar_one_or_none() 
+
+    return chatroom
+
+async def get_senders_messages_by_current_user_id(
+        current_user_id: int,
+        session: AsyncSession
+):
+    query = await session.execute(
+        select(UserAccount)
+        .join(Message.sender_user)
+        .where(Message.receiver_user_id == current_user_id)
+    )
+    senders = query.unique().scalars().all()
+
+    return senders
+
+async def get_messages_by_chatroom_id(
+    chatroom_id: int,
+    session: AsyncSession
+) -> List[Message]:
+    query = await session.execute(
+        select(Message)
+        .join(Message.chatroom)
+        .where(Chatroom.id == chatroom_id)
+        .order_by(Message.send_at.asc()) # old = asc
+    )
+    messages = query.unique().scalars().all()
+
+    return messages
+
+async def set_chatroom_as_read(
+    chatroom_id: int,
+    session: AsyncSession
+):
+    chatroom= await get_chatroom_by_id(chatroom_id, session)
+    chatroom.is_unread_receiver_user = 0
+    await session.commit()
 
 # UPDATE
 async def set_email_confirmation_token(user_id: int, token: str, expiration_time: datetime, session: AsyncSession):
